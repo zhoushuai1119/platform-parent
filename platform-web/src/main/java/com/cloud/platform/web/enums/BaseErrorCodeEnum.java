0103a0bff08ce811e6881ae30be8beb0674a2217
zhoushuai <zs11195310@163.com>
2022-05-05 23:53:19 +0800

    添加系统错误码

--- platform-web/src/main/java/com/cloud/platform/web/enums/BaseErrorCodeEnum.java
+++ platform-web/src/main/java/com/cloud/platform/web/enums/BaseErrorCodeEnum.java
@@ -1,5 +1,6 @@
 package com.cloud.platform.web.enums;
 
+import com.cloud.platform.common.exception.BaseExceptionCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,18 +12,34 @@ import lombok.Getter;
  */
 @AllArgsConstructor
 @Getter
-public enum BaseErrorCodeEnum {
+public enum BaseErrorCodeEnum implements BaseExceptionCode {
 
     SUCCESS("000000", "success"),
     SYSTEM_ERROR("100000", "system error"),
     PARAM_ERROR("100001", "param error"),
     JSON_PARSER_ERROR("100002", "json parse error"),
-    CONVERSION_FAILED_ERROR("100003", "conversion failed exception"),
-    ILLEGAL_ARGUMENT_ERROR("100004", "illegal argument exception"),
-    TOKEN_VERIFICATION_ERROR("100005", "token  verification exception")
+    FEIGN_CLIENT_ERROR("100003", "feign invoke fail"),
+    CONVERSION_FAILED_ERROR("100004", "conversion failed exception"),
+    ILLEGAL_ARGUMENT_ERROR("100005", "illegal argument exception"),
+    TOKEN_VERIFICATION_ERROR("100006", "token  verification exception")
     ;
 
     private String code;
     private String message;
+
+    @Override
+    public String getErrorCode() {
+        return code;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return message;
+    }
+
+    @Override
+    public String getErrorTips() {
+        return message;
+    }
 
 }
