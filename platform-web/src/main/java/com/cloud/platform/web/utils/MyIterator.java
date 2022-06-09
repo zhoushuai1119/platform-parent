1c103c69c8e2faa3bb9620a4c3b97828e31be40b
zhoushuai <zs11195310@163.com>
2022-06-09 17:35:24 +0800

    自定义迭代器修改

--- platform-web/src/main/java/com/cloud/platform/web/utils/MyIterator.java
+++ platform-web/src/main/java/com/cloud/platform/web/utils/MyIterator.java
@@ -6,17 +6,31 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * @Description:
+ * @Description: 自定义迭代器MyIterator
  * @Author: ZhouShuai
  * @Date: 2021-06-04 10:49
  */
-public abstract  class MyIterator<T> implements Iterator<T> {
+public abstract class MyIterator<T> implements Iterator<T> {
 
-    private int start = 1;
-    private int size = 50;
-
+    /**
+     * 页码
+     */
+    private int pageIndex = 1;
+    /**
+     * 页数
+     */
+    private int pageSize = 1000;
+    /**
+     * 当前索引
+     */
     private int currentIndex;
+    /**
+     * 是否有更多数据
+     */
     private boolean hasMore = true;
+    /**
+     * 数据集合
+     */
     private List<T> list;
 
     public MyIterator() {
@@ -25,21 +39,20 @@ public abstract  class MyIterator<T> implements Iterator<T> {
 
     @Override
     public boolean hasNext() {
-        //当前的数据已经加载完毕，尝试加载下一批
         if (!hasMore) {
             return false;
         }
-        list = load(start, size);
+        list = loadData(pageIndex, pageSize);
+        //没有加载到数据，结束
         if (CollectionUtils.isEmpty(list)) {
-            // 没有加载到数据，结束
             return false;
         }
-        if (list.size() < size) {
-            // 返回条数小于限制条数，表示还有更多的数据可以加载
+        //返回条数小于限制条数，表示没有更多的数据可以加载
+        if (list.size() < pageSize) {
             hasMore = false;
         }
         currentIndex = 0;
-        this.start ++;
+        this.pageIndex++;
         return true;
     }
 
@@ -52,6 +65,15 @@ public abstract  class MyIterator<T> implements Iterator<T> {
         return list;
     }
 
-    public abstract List<T> load(int start, int size);
+    /**
+     * 初始化参数设置
+     */
+    public void init() {
+        this.pageIndex = 1;
+        hasMore = true;
+        list = null;
+    }
+
+    public abstract List<T> loadData(int pageIndex, int pageSize);
 
 }
