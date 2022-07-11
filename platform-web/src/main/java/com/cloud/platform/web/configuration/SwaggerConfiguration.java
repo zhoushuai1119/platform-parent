dea977d26bd351b983b737b1c40cb02305ce85de
zhoushuai <shuai.zhou@avatr.com>
2022-07-11 18:24:56 +0800

    添加swagger鉴权配置

--- platform-web/src/main/java/com/cloud/platform/web/configuration/SwaggerConfiguration.java
+++ platform-web/src/main/java/com/cloud/platform/web/configuration/SwaggerConfiguration.java
@@ -8,14 +8,18 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -47,8 +51,7 @@ public class SwaggerConfiguration {
         } else {
             select.apis(RequestHandlerSelectors.any());
         }
-
-        select.build();
+        select.build().securitySchemes(securitySchemes());
         return docket;
     }
 
@@ -64,6 +67,16 @@ public class SwaggerConfiguration {
                 .termsOfServiceUrl(swaggerProperties.getServiceUrl())
                 .version(swaggerProperties.getVersion())
                 .build();
+    }
+
+
+    /**
+     * 鉴权
+     * @return
+     */
+    private List<SecurityScheme> securitySchemes() {
+        ApiKey apiKey = new ApiKey("Authorization", "login-token", "header");
+        return Collections.singletonList(apiKey);
     }
 
 }
