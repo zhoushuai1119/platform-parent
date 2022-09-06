b49791c722102fdd6552be6b3cb4b26a0a3b719e
zhoushuai <shuai.zhou@avatr.com>
2022-09-06 17:32:04 +0800

    依赖配置修改

--- platform-web/src/main/java/com/cloud/platform/web/configuration/RedissonConfiguration.java
+++ platform-web/src/main/java/com/cloud/platform/web/configuration/RedissonConfiguration.java
@@ -9,13 +9,10 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @description: Redisson配置
